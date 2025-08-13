594923294ef09adac3f0144aec36ec005f7b4766
Ayush Sharma <=>
2025-08-13 16:39:57 +0530

    injected Lombok

--- src/main/java/com/firstapi/demo/entity/JournalEntry.java
+++ src/main/java/com/firstapi/demo/entity/JournalEntry.java
@@ -2,28 +2,19 @@ package com.firstapi.demo.entity;
 
 import java.time.LocalDateTime;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "journalEntries")
+@Data
 public class JournalEntry {
+
     @Id
     private ObjectId id;
     private String title;
     private String content;
     private LocalDateTime date;
 
-    // Getters and setters
-    public ObjectId getId() { return id; }
-    public void setId(ObjectId id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-
-    public LocalDateTime getDate() { return date; }
-    public void setDate(LocalDateTime date) { this.date = date; }
 }
